23272768eb55f50c6feabec461843ca37f9a1bff
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-02-17 21:02:35 +0100

    Add the login form

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Login.module.css'
 
 function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(password, email);
+  }
+
   return (
-    <div>Login</div>
+    <form className={styles['login-form']}
+    onSubmit={handleSubmit}
+    >
+      <h2>Login</h2>
+
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          placeholder="Your Email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          placeholder="Your Password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      <button className="btn">Submit</button>
+    </form>
   )
 }
 

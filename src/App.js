92e7fa3998ca85d36aba94c1b97fa8d3e80976eb
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-02-17 19:44:30 +0100

    Setup the router with react router dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+
+
 function App() {
   return (
-    <div>Hello</div>
+    <>
+    <Routes>
+      <Route
+        exact
+        path="/"
+        element={(<Login/>)}
+      />
+
+      <Route
+        path="/home"
+        element={(<Home/>)}
+      />
+
+      <Route
+        path="/signup"
+        element={(<Signup/>)}
+      />
+    </Routes>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const append = document.getElementById('root')
+const root = createRoot(append);
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );

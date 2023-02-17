bc5c98f6afbc371f00bb05d23cbbd9520786559c
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-02-17 20:24:51 +0100

    Add the Navbar

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
-
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+    <Navbar />
     <Routes>
+      <Route
+      path="/login"
+      element={(<Login/>)}
+      />
+      
       <Route
         exact
         path="/"
-        element={(<Login/>)}
-      />
-
-      <Route
-        path="/home"
         element={(<Home/>)}
       />
 

da0e9ab4f599933b03b3caf8572613ff514e727c
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-02-20 19:26:52 +0100

    Added authIsReady to show the user account

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-
+import { useAuthContext } from "./hook/useAuthContext";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const { authIsReady } = useAuthContext()
   return (
     <>
-    <Navbar />
+    { authIsReady && (<>
+      <Navbar />
     <Routes>
       <Route
       path="/login"
@@ -26,6 +28,8 @@ function App() {
         element={(<Signup/>)}
       />
     </Routes>
+    </>)
+    }
     </>
   );
 }

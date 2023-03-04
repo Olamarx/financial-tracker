eec3f35c466241ec505f358815826a459c6dee31
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-03-04 02:12:08 +0100

    Add transaction list

---
+++ src/pages/home/TransactionList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './Home.module.css'
+
+function TransactionList({ transactions }) {
+  return (
+    <ul className={styles.transactions}>
+      { transactions.map((transaction) => (
+        <li key={transaction.id}>
+          <p className={styles.name}>{transaction.name}</p>
+          <p className={styles.amount}>${transaction.amount}</p>
+        </li>
+      )) }
+    </ul>
+  )
+}
+
+export default TransactionList

1360c43212eec48085c8c30e59b67e9e1f6a0a15
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-03-11 16:44:41 +0100

    Add delete functionality

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import styles from './Home.module.css'
+import { useFirestore } from '../../hook/useFirestore'
 
 function TransactionList({ transactions }) {
+const { deleteDocument } = useFirestore('transactions')
+
   return (
     <ul className={styles.transactions}>
       { transactions.map((transaction) => (
         <li key={transaction.id}>
           <p className={styles.name}>{transaction.name}</p>
           <p className={styles.amount}>${transaction.amount}</p>
+          <button onClick={() => deleteDocument(transaction.id)}>X</button>
         </li>
       )) }
     </ul>

056283fe57cddb411c7dfc482b8ffefbb829f198
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-03-11 16:38:06 +0100

    query to order and what to get for user

--- src/hook/useCollection.js
+++ src/hook/useCollection.js
@@ -1,13 +1,21 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import { projectFirestore } from "../firebase/config"
 
-export const useCollection = (collection) => {
+export const useCollection = (collection, _query, _orderBy) => {
   const [error, setError] = useState(null)
   const [documents, setDocuments] = useState(null)
+  const query = useRef(_query).current
+  const orderBy = useRef(_orderBy).current
 
   useEffect(() => {
     let ref = projectFirestore.collection(collection)
 
+    if (query) {
+      ref = ref.where(...query)
+    }
+    if (orderBy) {
+      ref = ref.orderBy(...orderBy)
+    }
     const unsubscribe = ref.onSnapshot((snapshot) => {
       let results = []
 
@@ -24,7 +32,7 @@ export const useCollection = (collection) => {
 
     return () => unsubscribe()
 
-  }, [collection])
+  }, [collection, query, orderBy])
 
   return { documents, error }
 }

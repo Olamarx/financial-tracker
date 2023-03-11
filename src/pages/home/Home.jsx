1620142e9934f5a07d41c84d06051fff3aee55ec
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2023-03-11 16:44:24 +0100

    Add useCollection from the transaction

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,7 +7,7 @@ import TransactionList from './TransactionList';
 
 function Home() {
   const { user } = useAuthContext()
-  const { documents, error } = useCollection('transactions')
+  const { documents, error } = useCollection('transactions', ['uid', '==', user.uid], ['createdAt', 'desc'])
   return (
     <div className={styles.container}>
       <div className={styles.content}>
